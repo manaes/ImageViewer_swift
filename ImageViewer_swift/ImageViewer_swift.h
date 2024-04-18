cce2101ebc0b704c839e4162290de13494ac77e6
wanny.park <wanny.park@pandora.tv>
2024-04-18 16:48:27 +0900

    Initial Commit

---
+++ ImageViewer_swift/ImageViewer_swift.h
@@ -0,0 +1,18 @@
+//
+//  ImageViewer_swift.h
+//  ImageViewer_swift
+//
+//  Created by manaes on 4/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageViewer_swift.
+FOUNDATION_EXPORT double ImageViewer_swiftVersionNumber;
+
+//! Project version string for ImageViewer_swift.
+FOUNDATION_EXPORT const unsigned char ImageViewer_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageViewer_swift/PublicHeader.h>
+
+
